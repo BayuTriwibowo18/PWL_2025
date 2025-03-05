d70b5aeb8a9e1a78722e148f59bc4f2e24c89b4a
Bayy <wibowobayu600@gmail.com>
2025-03-05 12:45:39 +0700

    Jobsheet3

---
+++ Minggu3/Jobsheet3/app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function User($id, $name){
+        return view('user.profile', compact('id','name'));
+    }
+
+    public function index(){
+
+        $data =
+        [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data);
+        
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+
+}
