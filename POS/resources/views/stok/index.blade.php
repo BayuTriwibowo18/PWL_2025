9e19f9fcedba991ce7cb679dde0f1707160741d3
Bayy <wibowobayu600@gmail.com>
2025-04-15 17:40:32 +0700

    revisi filtering

--- POS/resources/views/stok/index.blade.php
+++ POS/resources/views/stok/index.blade.php
@@ -1,19 +1,24 @@
-<!-- resources/views/stok/index.blade.php -->
 @extends('layouts.template')
 
 @section('content')
-    <div class="card">
+    <div class="card card-outline card-primary">
         <div class="card-header">
-            <h3 class="card-title">Daftar Stok Barang</h3>
+            <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <button onclick="modalAction('{{ url('/stok/import') }}')" class="btn btn-info">Import Stok</button>
-                <a href="{{ url('/stok/export_excel') }}" class="btn btn-primary"><i class="fa fa-file- excel"></i> Export Stok</a>
-                <a href="{{ url('/stok/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file- pdf"></i> Export Stok (pdf)</a>
+                <a href="{{ url('/stok/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"></i>Export Stok</a>
+                <a href="{{ url('/stok/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export Stok</a>
                 <button onclick="modalAction('{{ url('/stok/create_ajax') }}')" class="btn btn-success">Tambah Data (Ajax)</button>
+
             </div>
         </div>
-        
         <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
             <div class="row">
                 <div class="col-md-12">
                     <div class="form-group row">
@@ -30,18 +35,10 @@
                     </div>
                 </div>
             </div>
-            @if(session('success'))
-                <div class="alert alert-success">{{ session('success') }}</div>
-            @endif
-            
-            @if(session('error'))
-                <div class="alert alert-danger">{{ session('error') }}</div>
-            @endif
-            
-            <table class="table table-bordered table-sm table-striped table-hover" id="table-stok">
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_stok">
                 <thead>
                     <tr>
-                        <th>ID</th>
+                        <th>No</th>
                         <th>Nama Barang</th>
                         <th>Nama User</th>
                         <th>Tanggal Stok</th>
@@ -49,89 +46,79 @@
                         <th>Aksi</th>
                     </tr>
                 </thead>
-                <tbody></tbody>
             </table>
         </div>
     </div>
-    <div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static" data-keyboard="false" data-width="75%"></div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data- backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div> 
 @endsection
+
+@push('css') 
+@endpush
 
 @push('js')
     <script>
-        function modalAction(url = '') {
-            $('#myModal').load(url, function() {
+        function modalAction(url = ''){
+            $('#myModal').load(url,function(){
                 $('#myModal').modal('show');
             });
         }
 
-        var tableStok;
         $(document).ready(function() {
-            tableStok = $('#table-stok').DataTable({
-                processing: true,
+            var dataStok = $('#table_stok').DataTable({
                 serverSide: true,
                 ajax: {
-                    url: "{{ url('stok/list') }}",
-                    type: "POST",
-                    dataType: "json",
-                    data: function(d) {
-                        d.filter_barang = $('.filter_barang').val();
+                    "url": "{{ url('stok/list') }}", 
+                    "dataType": "json",
+                    "type": "POST",
+                    "data": function (d) {
+                        d.user_id = $('#user_id').val(); // Filter berdasarkan user_id
+                        d._token = "{{ csrf_token() }}"; // Tambahkan CSRF token
                     }
                 },
                 columns: [
-                    { 
-                        data: "DT_RowIndex", 
-                        className: "text-center", 
-                        width: "5%", 
+                    {
+                        data: "DT_RowIndex",
+                        className: "text-center",
+                        width: "5%",
                         orderable: false, 
-                        searchable: false 
-                    },
-                    { 
-                        data: "barang.barang_nama", 
-                        className: "", 
-                        width: "25", 
-                        orderable: true, 
-                        searchable: true 
+                        searchable: false
                     },
                     {
-                        data: "user.nama", 
+                        data: "barang.barang_nama", // Mengambil nama barang dari relasi
                         className: "",
-                        width: "25",
                         orderable: false,
                         searchable: false
                     },
-                    { 
-                        data: "stok_tanggal", 
-                        className: "", 
-                        width: "25%", 
-                        orderable: true, 
-                        searchable: true 
+                    {
+                        data: "user.nama", // Mengambil nama user dari relasi
+                        className: "",
+                        orderable: false,
+                        searchable: false
                     },
-                    { 
-                        data: "stok_jumlah", 
-                        className: "", 
-                        width: "20%", 
-                        orderable: true, 
-                        searchable: false 
+                    {
+                        data: "stok_tanggal",
+                        className: "",
+                        orderable: true,
+                        searchable: true
                     },
-                    { 
-                        data: "aksi", 
-                        className: "text-center", 
-                        width: "20%", 
-                        orderable: false, 
-                        searchable: false 
+                    {
+                        data: "stok_jumlah",
+                        className: "",
+                        orderable: true,
+                        searchable: false
+                    },
+                    {
+                        data: "aksi",
+                        className: "",
+                        orderable: false,
+                        searchable: false
                     }
                 ]
             });
 
-            $('#table-stok_filter input').unbind().bind().on('keyup', function(e) {
-                if (e.keyCode == 13) { 
-                    tableStok.search(this.value).draw();
-                }
-            });
-
-            $('.filter_barang').change(function() {
-                tableStok.draw();
+            $('#user_id').on('change', function() {
+                dataStok.ajax.reload();
             });
         });
-    </script>
+    </script> 
 @endpush
--- POS/resources/views/user/index.blade.php
+++ POS/resources/views/user/index.blade.php
@@ -1,47 +1,40 @@
 @extends('layouts.template')
 
 @section('content')
-    <div class="card">
+    <div class="card card-outline card-primary">
         <div class="card-header">
-            <h3 class="card-title">Daftar User</h3>
+            <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <button onclick="modalAction('{{ url('/user/import') }}')" class="btn btn-info">Import User</button>
-                <a href="{{ url('/user/export_excel') }}" class="btn btn-primary"><i class="fa fa-file- excel"></i> Export User</a>
-                <a href="{{ url('/user/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file- pdf"></i> Export User (pdf)</a>
+                <a href="{{ url('/user/export_excel') }}" class="btn btn-primary"><i class="fa fa-file-excel"></i>Export User</a>
+                <a href="{{ url('/user/export_pdf') }}" class="btn btn-warning"><i class="fa fa-file-pdf"></i> Export User</a>
                 <button onclick="modalAction('{{ url('/user/create_ajax') }}')" class="btn btn-success">Tambah Data (Ajax)</button>
             </div>
         </div>
-        
         <div class="card-body">
-            <!-- Filter Data -->
-            <div id="filter" class="form-horizontal filter-date p-2 border-bottom mb-2">
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="form-group form-group-sm row text-sm mb-0">
-                            <label for="filter_date" class="col-md-1 col-form-label">Filter</label>
-                            <div class="col-md-3">
-                                <select name="filter_level" class="form-control form-control-sm filter_level">
-                                    <option value="">- Semua -</option>
-                                    @foreach($level as $l)
-                                        <option value="{{ $l->level_id }}">{{ $l->level_nama }}</option>
-                                    @endforeach
-                                </select>
-                                <small class="form-text text-muted">Level Pengguna</small>
-                            </div>
+            @if (session('success'))
+                <div class="alert alert-success">{{ session('success') }}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger">{{ session('error') }}</div>
+            @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter:</label>
+                        <div class="col-3">
+                            <select class="form-control" id="level_id" name="level_id" required>
+                                <option value="">- Semua</option>
+                                @foreach ($level as $item)
+                                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Level Pengguna</small>
                         </div>
                     </div>
                 </div>
             </div>
-            
-            @if(session('success'))
-                <div class="alert alert-success">{{ session('success') }}</div>
-            @endif
-            
-            @if(session('error'))
-                <div class="alert alert-danger">{{ session('error') }}</div>
-            @endif
-            
-            <table class="table table-bordered table-sm table-striped table-hover" id="table-user">
+            <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
                     <tr>
                         <th>No</th>
@@ -51,82 +44,75 @@
                         <th>Aksi</th>
                     </tr>
                 </thead>
-                <tbody></tbody>
             </table>
         </div>
     </div>
-    <div id="myModal" class="modal fade animate shake" tabindex="-1" data-backdrop="static" data-keyboard="false" data-width="75%"></div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data- backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
+
+@push('css') 
+@endpush
 
 @push('js')
     <script>
-        function modalAction(url = '') {
-            $('#myModal').load(url, function() {
+        function modalAction(url = ''){
+            $('#myModal').load(url,function(){
                 $('#myModal').modal('show');
             });
         }
 
-        var tableUser;
         $(document).ready(function() {
-            tableUser = $('#table-user').DataTable({
-                processing: true,
+            var dataUser = $('#table_user').DataTable
+            ({
+                // serverSide: true, jika ingin menggunakan server side processing 
                 serverSide: true,
                 ajax: {
-                    url: "{{ url('user/list') }}",
-                    type: "POST",
-                    dataType: "json",
-                    data: function(d) {
-                        d.filter_level = $('.filter_level').val();
+                    "url": "{{ url('user/list') }}", 
+                    "dataType": "json",
+                    "type": "POST",
+                    "data": function (d){
+                        d.level_id = $('#level_id').val();
                     }
                 },
-                columns: [
-                    { 
-                        data: "DT_RowIndex", 
-                        className: "text-center", 
-                        width: "5%", 
+                columns: 
+                [
+                    {
+                        // nomor urut dari laravel datatable addIndexColumn() 
+                        data: "DT_RowIndex",
+                        className: "text-center",
                         orderable: false, 
-                        searchable: false 
-                    },
-                    { 
-                        data: "username", 
+                        searchable: false
+                    },{
+                        data: "username",  
+                        className: "",
+                        // orderable: true, jika ingin kolom ini bisa diurutkan 
+                        orderable: true,
+                        // searchable: true, jika ingin kolom ini bisa dicari 
+                        searchable: true
+                    },{
+                        data: "nama",
                         className: "", 
-                        width: "25%", 
                         orderable: true, 
-                        searchable: true 
-                    },
-                    { 
-                        data: "nama", 
+                        searchable: true
+                    },{
+                        // mengambil data level hasil dari ORM berelasi 
+                        data: "level.level_nama",
                         className: "", 
-                        width: "35%", 
-                        orderable: true, 
-                        searchable: true 
-                    },
-                    { 
-                        data: "level.level_nama", 
-                        className: "", 
-                        width: "20%", 
-                        orderable: true, 
-                        searchable: false 
-                    },
-                    { 
-                        data: "aksi", 
-                        className: "text-center", 
-                        width: "15%", 
                         orderable: false, 
-                        searchable: false 
+                        searchable: false
+                    },{
+                        data: "aksi",
+                        className: "", 
+                        orderable: false, 
+                        searchable: false
                     }
                 ]
             });
 
-            $('#table-user_filter input').unbind().bind().on('keyup', function(e) {
-                if (e.keyCode == 13) { // Enter key
-                    tableUser.search(this.value).draw();
-                }
+            $('#level_id').on('change', function(){
+                dataUser.ajax.reload();
             });
-
-            $('.filter_level').change(function() {
-                tableUser.draw();
-            });
+            
         });
-    </script>
+    </script> 
 @endpush
